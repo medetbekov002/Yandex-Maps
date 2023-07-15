7bd37095335cdcc1b06a37d85216c4c0e81ceb69
theMIslam <valvekakaxa@gmail.com>
2023-07-15 12:29:24 +0600

    done

---
+++ app/src/main/java/com/example/firsttest/Model.kt
@@ -0,0 +1,4 @@
+package com.example.firsttest
+
+class Model(val title:String, val lat:String, val lon:String) {
+}
---
+++ app/src/main/java/com/example/firsttest/MyListAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.firsttest
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+
+class MyListAdapter(var mCtx: Context, var resource:Int, var items:List<Model>) :ArrayAdapter<Model>(mCtx, resource,items) {
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val LayoutInflater:LayoutInflater = LayoutInflater.from(mCtx)
+        val view:View = LayoutInflater.inflate(resource, null)
+        val textView:TextView = view.findViewById(R.id.idtext)
+        val lat:TextView = view.findViewById(R.id.lat)
+        val lon:TextView = view.findViewById(R.id.lon)
+        var mItems:Model = items[position]
+        textView.text = mItems.title
+        lat.text = mItems.lat
+        lon.text = mItems.lon
+        return view
+    }
+}
